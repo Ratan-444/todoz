368e7f023b57fe7955e89d4cb1f18282af5ff19d
Ratan-444 <131332225+Ratan-444@users.noreply.github.com>
2024-12-09 21:58:26 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,7 +6,7 @@ import './App.css';
 
 function App() {
     const [todos, setTodos] = useState([]);
-    const API_URL = 'https://todoz-backend-aru0eq5xp-ratans-projects-bda0b515.vercel.app';
+    const API_URL = 'https://todoz-backend.vercel.app/';
 
     const fetchTodos = async () => {
         try {
