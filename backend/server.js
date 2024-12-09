8e645015bda6663d5fd74e82a285838c7d77c9df
Ratan-444 <131332225+Ratan-444@users.noreply.github.com>
2024-12-09 21:36:43 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -12,6 +12,7 @@ const PORT = process.env.PORT || 5000;
 app.use(cors({
   origin: 'https://todoz-tkr9.vercel.app', // Allow your frontend's domain
   methods: ['GET', 'POST', 'DELETE', 'PUT'],  // Allow these methods
+  allowedHeaders: ['Content-Type', 'Authorization']  // Add required headers
 }));
 app.use(bodyParser.json());
 app.use('/api/todos', todoRoutes);
